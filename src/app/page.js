1be2349c82e8eb8c6af90b68997121576ff7c74d
James Isom <jamesisom310@gmail.com>
2023-09-28 21:58:09 -0400

    Created Account Creation page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
+    <div>
     <h1>HELLO GROUP 1!</h1>
+    <Link href="/account_creation">Account Creation Page</Link> {/* Temporary navigation to account creation page */}
+    </div>
   )
 }
