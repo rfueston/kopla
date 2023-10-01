830fc1beabd2bd3fc686a9ca2d6d849dafc98fe1
rfueston <rfueston@gmail.com>
2023-10-01 08:47:56 -0400

    KOPLA-14
    -pickup lane page added
    -map added to pick lane
    -update CSS to remove background due to issue with repeating after selecting back buttom
    
    TODO: finished GPS integration

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,28 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
-  return (
+    return (
+        <div>
+            <h1>HELLO GROUP 1!</h1>
+
     <div>
-    <h1>HELLO GROUP 1!</h1>
-    <Link href="/account_creation">Account Creation Page</Link> {/* Temporary navigation to account creation page */}
-    <Link href="/login">Login</Link> {/* Temporary navigation to account creation page */}
-    <Link href="/firebase">firebase</Link> {/* Temporary navigation to account creation page */}
+        <Link href="/account_creation">Account Creation
+            Page</Link> {/* Temporary navigation to account creation page */}
     </div>
-  )
+
+    <div>
+        <Link href="/login">Login</Link> {/* Temporary navigation to account creation page */}
+    </div>
+
+    <div>
+        <Link href="/firebase">firebase</Link> {/* Temporary navigation to account creation page */}
+    </div>
+
+    <div>
+        <Link href="/pickup_lane">Pickup Lane</Link> {/* Temporary navigation to GPS page */}
+    </div>
+
+
+</div>
+)
 }
