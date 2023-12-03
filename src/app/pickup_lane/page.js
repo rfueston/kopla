2f18ba348b102947ad181ed2244cdb14b26a291f
James Isom <jamesisom310@gmail.com>
2023-12-03 14:07:46 -0500

    Removed scan QR code button for parents

--- src/app/pickup_lane/page.js
+++ src/app/pickup_lane/page.js
@@ -390,7 +390,7 @@ export default function PickLane() {
               </div>
             )}
 
-            {(!profileData.isParent || profileData.isAdmin) && (
+            {(!profileData.isParent) && (
               <div>
                 <CameraWithQRCodeScanner getParentData={getParentData}/>
             </div>
